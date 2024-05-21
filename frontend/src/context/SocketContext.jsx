eae5274926e2b254f709af8bde5ad7fe942c381c
rakshitsharma3103 <rakshitsharma3103@gmail.com>
2024-05-21 17:23:18 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://localhost:5000", {
+			const socket = io("https://chatbuzz-chatapp.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
