c6b8d0222e5b78ac22416169ec057113a5e57497
rakshitsharma3103 <rakshitsharma3103@gmail.com>
2024-05-21 16:59:11 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 // I have created/initialized npm init in root folder so that I can run/access both backend & frontend.
+import path from "path";
+
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,8 +14,9 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();   //without using this i cannot access environment variables from .env file.
 
-
 const PORT = process.env.PORT || 5000; 
+
+const __dirname = path.resolve();
 
 //Middlewares (Imp Note in Bottom)
 app.use(express.json());  //to parse the incoming requests with JSON payloads. (from req.body), used for auth.controller.js file
@@ -23,8 +26,15 @@ app.use("/api/auth", authRoutes);    //using this i can access all routes starti
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))      //used to serve static files such as html/css, JS, images, sound file etc.
+// when we run build on our frontend then all files will save in dist
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 // app.get("/", (req,res)=> {
-//     res.send("Hii dekha ho gya na!!")            
+//     res.send("Hii!!")            
 // }),
 
 server.listen(PORT, () => {
